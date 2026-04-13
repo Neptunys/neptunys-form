4f40b1ff7b22ef7e464902888547ceda19d8a4dd
Neptunys <contact@neptunys.com>
2026-04-13 21:07:37 +0200

    Force MailQueue timeout override

--- vendor/heyform-upstream/packages/server/src/queue/index.ts
+++ vendor/heyform-upstream/packages/server/src/queue/index.ts
@@ -1,4 +1,4 @@
-import { BullModule } from '@nestjs/bull'
+import { BullModule, BullModuleOptions } from '@nestjs/bull'
 
 import { BullOptionsFactory } from '@config'
 
@@ -7,6 +7,24 @@ import { IntegrationQueue } from './integration-queue'
 import { MailQueue } from './mail.queue'
 import { SubmissionNotificationQueue } from './submission-notification.queue'
 import { TranslateFormQueue } from './translate-form.queue'
+
+const MAIL_QUEUE_TIMEOUT_MS = 5 * 60 * 1000
+
+async function getQueueOptions(queueName: string): Promise<BullModuleOptions> {
+  const options = await BullOptionsFactory()
+
+  if (queueName !== 'MailQueue') {
+    return options
+  }
+
+  return {
+    ...options,
+    defaultJobOptions: {
+      ...(options.defaultJobOptions ?? {}),
+      timeout: MAIL_QUEUE_TIMEOUT_MS
+    }
+  }
+}
 
 export const QueueProviders = {
   FormReportQueue,
@@ -19,6 +37,6 @@ export const QueueProviders = {
 export const QueueModules = Object.keys(QueueProviders).map(queueName => {
   return BullModule.registerQueueAsync({
     name: queueName,
-    useFactory: BullOptionsFactory
+    useFactory: () => getQueueOptions(queueName)
   })
 })
